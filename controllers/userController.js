d7113fa21e4ffa9cdb62e97f2342d591985ee6a4
Srinath20 <srinathgollapinni>
2024-06-20 09:41:18 +0530

    Password secured

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql2');
+const bcrypt = require('bcrypt');
 
 const db = mysql.createConnection({
   host: 'localhost',
@@ -12,19 +13,27 @@ db.connect(err => {
   console.log('Connected to the database.');
 });
 
-exports.signupUser = (req, res) => {
+exports.signupUser = async (req, res) => {
   const { name, email, password } = req.body;
   const checkEmailSql = 'SELECT * FROM users WHERE email = ?';
-  db.query(checkEmailSql, [email], (err, results) => {
+  
+  db.query(checkEmailSql, [email], async (err, results) => {
     if (err) throw err;
     if (results.length > 0) {
       return res.status(400).json({ error: 'Email already taken' });
     } else {
-      const sql = 'INSERT INTO users (name, email, password) VALUES (?, ?, ?)';
-      db.query(sql, [name, email, password], (err, result) => {
-        if (err) throw err;
-        res.json({ id: result.insertId, name, email });
-      });
+      try {
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        const sql = 'INSERT INTO users (name, email, password) VALUES (?, ?, ?)';
+        
+        db.query(sql, [name, email, hashedPassword], (err, result) => {
+          if (err) throw err;
+          res.json({ id: result.insertId, name, email });
+        });
+      } catch (err) {
+        res.status(500).json({ error: 'Error hashing password' });
+      }
     }
   });
 };
@@ -32,13 +41,16 @@ exports.signupUser = (req, res) => {
 exports.loginUser = (req, res) => {
   const { email, password } = req.body;
   const sql = 'SELECT * FROM users WHERE email = ?';
-  db.query(sql, [email], (err, results) => {
+  
+  db.query(sql, [email], async (err, results) => {
     if (err) throw err;
     if (results.length === 0) {
       return res.status(404).json({ error: 'User not found.' });
     } else {
       const user = results[0];
-      if (user.password === password) {
+      const match = await bcrypt.compare(password, user.password);
+      
+      if (match) {
         res.json({ id: user.id, name: user.name, email: user.email });
       } else {
         res.status(400).json({ error: 'User not authorized' });
